7d31aa243366c2d6b1248013b129aca30a1f206e
bgiddwani-ai <bgiddwani@gmail.com>
2025-12-30 17:10:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def parse_requirements(filename):
         return f.read().splitlines()
         
 setup(
-    name='dots_ocr',  
+    name='nv_ocr',  
     version='1.0', 
     packages=find_packages(),  
     include_package_data=True,  
